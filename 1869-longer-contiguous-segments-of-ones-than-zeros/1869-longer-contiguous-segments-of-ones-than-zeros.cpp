0472e6a433c7f230f1b16ab30303f5a149a483cd
Rutuja Maruti Kumbhar <rutujakumbhar.prof@gmail.com>
2026-08-01 18:50:21 +0530

    Time: 0 ms (100%), Space: 8.1 MB (96.55%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -1,6 +1,49 @@
+// class Solution {
+// public:
+//     bool checkZeroOnes(string s) {
+            ////Solution 1
+//         int cnt=0;
+//         int bnt=0;
+//         int o =0;
+//         int z=0;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='1'){
+//                 cnt+=1;
+//             }
+//             else{
+//                 cnt=0;
+//             }
+//             o=max(o,cnt);
+            
+
+
+//         }
+//         cout<<o;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='0'){
+//                 bnt+=1;
+//             }
+//             else{
+//                 bnt=0;
+//             }
+//             z=max(z,bnt);
+            
+
+
+//         }
+//         cout<<z;
+//         if(o>z){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool checkZeroOnes(string s) {
+        //Solution 2
         int cnt=0;
         int bnt=0;
         int o =0;
@@ -8,29 +51,18 @@ public:
         for(int i=0;i<s.size();i++){
             if(s[i]=='1'){
                 cnt+=1;
+                bnt=0;
             }
             else{
                 cnt=0;
-            }
-            o=max(o,cnt);
-            
-
-
-        }
-        cout<<o;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='0'){
                 bnt+=1;
             }
-            else{
-                bnt=0;
-            }
+            o=max(o,cnt);
             z=max(z,bnt);
             
 
-
         }
-        cout<<z;
+        
         if(o>z){
             return true;
         }
