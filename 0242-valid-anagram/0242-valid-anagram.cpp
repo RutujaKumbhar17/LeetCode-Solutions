d71543886d658f7a7f0af6a2763615e4cd0a0171
Rutuja Maruti Kumbhar <rutujakumbhar.prof@gmail.com>
2026-09-12 00:33:04 +0530

    Time: 0 ms (100%), Space: 10.1 MB (11.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        //SOLUTION 1:
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s.size()!=t.size()){
+        //     return false;
+        // }
+        // else{
+        //     for(int i=0;i<s.size();i++){
+        //     if(s[i]!=t[i])
+        //         return false;
+        // }
+        // }
+        // return true;
+
+        //SOLUTION 2:
+        unordered_map<char,int>s1;
+        unordered_map<char,int>t1;
+
         if(s.size()!=t.size()){
             return false;
         }
         else{
-            for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i])
+            for(char ch:s){
+            s1[ch]++;
+        }
+        for( char ch:t){
+            t1[ch]++;
+        }
+        for(char ch : s){
+            if(s1[ch]!=t1[ch]){
                 return false;
+            }
         }
+
         }
+
+        
         return true;
         
     }
